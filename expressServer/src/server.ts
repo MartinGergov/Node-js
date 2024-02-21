b25593440708e39a3ac298785d9ab81f0e3ac293
Martin Gergov <martindgergov@gmail.com>
2024-02-21 18:50:18 +0100

    Ejercicio CRUD with dummy database

--- expressServer/src/server.ts
+++ expressServer/src/server.ts
@@ -3,7 +3,7 @@ import "express-async-errors";
 import morgan from "morgan";
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -19,6 +19,40 @@ let planets: Planets = [
   { id: 1, name: "Earth" },
   { id: 2, name: "Mars" },
 ];
+
+app.get("/api/planets", (req, res) => {
+  res.status(200).json(planets);
+});
+
+app.get("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const planet = planets.find((p) => p.id === Number(id));
+
+  res.status(200).json(planet);
+});
+
+app.post("/api/planets", (req, res) => {
+  const { id, name } = req.body;
+  const newPlanet = { id, name };
+  planets = [...planets, newPlanet];
+
+  res.status(201).json({ msg: "The planet was created" });
+});
+
+app.put("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  planets = planets.map((p) => (p.id === Number(id) ? { ...p, name } : p));
+
+  res.status(200).json({ msg: "The planet was updated" });
+});
+
+app.delete("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  planets = planets.filter((p) => p.id !== Number(id));
+
+  res.status(200).json({ msg: "The planet was deleted" });
+});
 
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`);
