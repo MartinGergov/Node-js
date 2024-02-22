18dcbae730d12f715075e5a835f8e8f0da84773b
Martin Gergov <martindgergov@gmail.com>
2024-02-22 12:57:38 +0100

    Ejercicio Add Controllers

---
+++ expressServer/dist/controllers/planets.js
@@ -0,0 +1,30 @@
+let planets = [
+    { id: 1, name: "Earth" },
+    { id: 2, name: "Mars" },
+];
+const getAll = (req, res) => {
+    res.status(200).json(planets);
+};
+const getOneById = (req, res) => {
+    const { id } = req.params;
+    const planet = planets.find((p) => p.id === Number(id));
+    res.status(200).json(planet);
+};
+const create = (req, res) => {
+    const { id, name } = req.body;
+    const newPlanet = { id, name };
+    planets = [...planets, newPlanet];
+    res.status(201).json({ msg: "The planet was created" });
+};
+const updateById = (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    planets = planets.map((p) => (p.id === Number(id) ? Object.assign(Object.assign({}, p), { name }) : p));
+    res.status(200).json({ msg: "The planet was updated" });
+};
+const deleteById = (req, res) => {
+    const { id } = req.params;
+    planets = planets.filter((p) => p.id !== Number(id));
+    res.status(200).json({ msg: "The planet was deleted" });
+};
+export { getAll, getOneById, create, updateById, deleteById };
