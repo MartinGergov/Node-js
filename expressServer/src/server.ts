6942e8b54b5ee7e23244f0fa84aec1d1559885c2
Martin Gergov <martindgergov@gmail.com>
2024-02-23 18:51:01 +0100

    Ejercicio Upload files

--- expressServer/src/server.ts
+++ expressServer/src/server.ts
@@ -7,7 +7,20 @@ import {
   create,
   updateById,
   deleteById,
+  createImage,
 } from "./controllers/planets.js";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
 
 const app = express();
 const port = 3001;
@@ -16,14 +29,12 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/api/planets", getAll);
-
 app.get("/api/planets/:id", getOneById);
-
 app.post("/api/planets", create);
-
 app.put("/api/planets/:id", updateById);
-
 app.delete("/api/planets/:id", deleteById);
+
+app.post("/api/planets/:id/image", upload.single("image"), createImage);
 
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`);
